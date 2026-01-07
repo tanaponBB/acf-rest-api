0771472d009de4839bc483f8eae009548783fad9
tanaponBB <tanapon@theneighbors.co>
2026-01-07 11:50:17 +0700

    UPDATE : Change Version

--- acf-rest-api.php
+++ acf-rest-api.php
@@ -4,7 +4,7 @@
  * Plugin Name: REST API Extended
  * Plugin URI: https://theneighbors.co/
  * Description: Extends WordPress REST API with ACF Options and GTM Tracking endpoints.
- * Version: 2.0.0
+ * Version: 2.0.1
  * Author: TanaponBB
  * Author URI: https://theneighbors.co/
  * Text Domain: acf-rest-api
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Plugin constants
-define('ACF_REST_API_VERSION', '2.0.0');
+define('ACF_REST_API_VERSION', '2.0.1');
 define('ACF_REST_API_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('ACF_REST_API_PLUGIN_URL', plugin_dir_url(__FILE__));
 
